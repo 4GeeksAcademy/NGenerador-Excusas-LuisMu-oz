74261d3e5c9bc4f8ab452ee307bee3fb1e65b481
Luis11prog <m_luisskt8@outlook.com>
2025-08-11 19:59:53 +0000

    [Generador de excusas] Agrege unas cuantas cosas de boostrap para que se vea distinto y la imagen como se puede ver.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import "bootstrap";
 import "./style.css";
 
+window.onload = function () {
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let who = ['Un gato callejero', 'Mi primo pequeño', 'El vecino loco', 'Un cuervo misterioso'];
+  let action = ['quemó', 'perdió', 'mojó', 'tiró por la ventana'];
+  let what = ['mi poyecto final', 'mis apuntes', 'la presentación', 'el informe que tenía listo'];
+  let when = ['justo antes de salir', 'en medio de la tormenta', 'cuando iba camino a clase', 'mientras me preparaba para venir', 'hace solo unos minutos'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomWho = who[Math.floor(Math.random() * who.length)]
+  let randomAction = action[Math.floor(Math.random() * action.length)]
+  let randomWhat = what[Math.floor(Math.random() * what.length)]
+  let randomWhen = when[Math.floor(Math.random() * when.length)]
+
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`
+
+  document.getElementById("excuse").innerHTML = excuse
+  console.log(excuse);
+
 };
